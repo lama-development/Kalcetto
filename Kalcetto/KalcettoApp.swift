b649a9d061a4d62d653c1ff840f99dac69235a04
lama-development <davide.lamarca04@gmail.com>
2025-11-12 19:01:01 +0100

    Initial Commit

---
+++ Kalcetto/KalcettoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KalcettoApp.swift
+//  Kalcetto
+//
+//  Created by Davide La Marca on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct KalcettoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
